833548697a15e8fd560fed70bfb320b0f21e4c8d
Mattis-Klein <mattiskleinbh@gmail.com>
2026-01-26 03:39:31 -0500

    Initial commit (refreshed)

---
+++ CodeClub/password.js
@@ -0,0 +1,16 @@
+            function checkPassword() {
+               var passwordBoxObject = document.getElementById("passwordBox");
+               var enteredPassword = passwordBoxObject.value;
+               console.log("user entered password:",enteredPassword);
+               
+               var sitePassword = "letmein";
+               if (enteredPassword == sitePassword) {
+               window.location.assign("Members.html");
+            } else {
+                // wrong password text is red, prompt to try again
+                console.log("wrong password"); 
+                document.getElementById("wrongPass").style.color = "red";
+                document.getElementById("wrongPass").innerHTML = "Wrong password, try again...";
+
+            }
+        }
